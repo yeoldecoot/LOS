8d3e3981e2bc5ac25b7b232e0c8e22c06bf66646
Bit Wise <troho2003@gmail.com>
2026-02-08 18:28:23 -0800

    allow TLA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	base: "/LOS/",
+	build: {
+		target: "esnext", // <- allows top-level await
+	},
 	server: {
 		port: 8080,
 		open: true,
